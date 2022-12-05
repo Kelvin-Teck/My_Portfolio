ed3f927f186209e1855c7a236535ab4515e0d6fe
Eneh Kelvin Chukwuemeka <kelvineneh4@gmail.com>
2022-12-05 15:58:42 +0100

    updated the portfolio region

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CV from '../../assets/Eneh_Kelvin_Chukwuemeka.pdf'
+import CV from '../../assets/ekc.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
